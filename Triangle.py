de9b35a20699044b50f12c6a4ac867b2da4460d9
Mohini Mayekar <mmayekar97@gmail.com>
2023-09-22 11:48:05 -0400

    Improved classify traingle logic

--- Triangle.py
+++ Triangle.py
@@ -27,31 +27,38 @@ def classifyTriangle(a,b,c):
       BEWARE: there may be a bug or two in this code
     """
 
+    # verify that all 3 inputs are integers and/ or float 
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not((isinstance(a,int) or isinstance(a,float)) and (isinstance(b,int) or isinstance(b,float)) and (isinstance(c,int) or isinstance(c,float))):
+        return 'InvalidInput';
+
     # require that the input values be >= 0 and <= 200
-    if a > 200 or b > 200 or c > 200:
+    if a >= 200 or b >= 200 or c >= 200: # added the missing equals condition for inputs
         return 'InvalidInput'
         
-    if a <= 0 or b <= b or c <= 0:
+    if a <= 0 or b <= 0 or c <= 0: # rectified condition for input b
         return 'InvalidInput'
     
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
+    
       
     # This information was not in the requirements spec but 
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b - c)) or (b >= (a - c)) or (c >= (a + b)):
+    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)): # rectified first two conditions
         return 'NotATriangle'
         
     # now we know that we have a valid triangle 
-    if a == b and b == a:
-        return 'Equilateral'
-    elif ((a * 2) + (b * 2)) == (c * 2):
-        return 'Right'
-    elif (a != b) and  (b != c) and (a != b):
-        return 'Scalene'
+    # A triangle can be Equilateral, Isosceles , Scalene and Right as well
+    triangle_type = ''
+    if a == b and b == c and c==a:
+        triangle_type = 'Equilateral'
+    elif (a != b) and  (b != c) and (c != a):
+        triangle_type = 'Scalene'
     else:
-        return 'Isoceles'
+        triangle_type = 'Isosceles'
+    
+    if(((a ** 2 + b ** 2) == c ** 2) or ((b ** 2 + c ** 2) == a ** 2) or ((a ** 2 + c ** 2) == b ** 2)):
+        triangle_type = 'Right ' + triangle_type
+    
+    return triangle_type
